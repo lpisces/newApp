8def3ab780f78783056af5761b74a9cd9566a3dc
lpisces <iamalazyrat@gmail.com>
2018-09-04 13:05:33 +0800

    init

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+	Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ],
 
     /*
